b65d49b26c95a23622ff363695a96ee874a71960
nbhoff@wisc.edu <>
2020-02-13 20:02:20 -0600

    Through M1-12

--- app/src/main/java/com/example/lab3milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab3milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button openDialogBtn = findViewById(R.id.openDialogBtn);
+        openDialogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog(){
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
     }
 }
